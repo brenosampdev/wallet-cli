70316ebd1952b270b8479d89545f671d5d9d2b1b
Breno <brenosg2310@gmail.com>
2026-03-28 18:06:47 -0300

    feat(goal): add goal command with subcommand documentation

--- src/main/java/org/example/commands/goal/Goal.java
+++ src/main/java/org/example/commands/goal/Goal.java
@@ -1,4 +1,27 @@
 package org.example.commands.goal;
 
-public class Goal {
+import org.example.core.CommandContext;
+import org.example.core.interfaces.Command;
+
+public class Goal implements Command {
+    @Override
+    public String name() {
+        return "goal";
+    }
+
+    @Override
+    public String info() {
+        return """
+                Goal command, used with subcommands:
+                - add: add a new goal
+                - rm: remove a goal
+                - update: update a goal
+                - listByName: list goals by name
+                - listAll: list all goals
+                """;
+    }
+
+    public void execute(CommandContext context) {
+        System.out.println(info());
+    }
 }
