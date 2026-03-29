430373265fc5030acea3dfe06b860fcec27cee1b
brancestack <nicribeirolis@gmail.com>
2026-03-29 15:36:20 -0300

    feat/add goals, middleware validation and fixes

--- src/main/java/org/example/core/middleware/core/MiddlewareBase.java
+++ src/main/java/org/example/core/middleware/core/MiddlewareBase.java
@@ -1,6 +1,6 @@
-package org.example.core.middleware.core;
+package org.example.core.middleware;
 
-public abstract class MiddlewareBase implements Middleware{
+public abstract class MiddlewareBase implements Middleware {
     protected Middleware next;
 
     public MiddlewareBase setNext(Middleware next){
