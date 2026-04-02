614a46fb4c6d4b6dfca46cd224d52288fc7f7984
Breno <brenosg2310@gmail.com>
2026-04-02 13:33:38 -0300

    feat(middleware): add HelpMiddleware to display info for non-leaf commands

---
+++ src/main/java/org/example/middleware/HelpMiddleware.java
@@ -0,0 +1,22 @@
+package org.example.middleware;
+
+import org.example.core.middleware.core.MiddlewareBase;
+import org.example.core.records.ResolvedCommand;
+
+public class HelpMiddleware extends MiddlewareBase {
+
+    @Override
+    public void handle(Object ctx) {
+        if(!(ctx instanceof ResolvedCommand data)){
+            next.handle(ctx);
+            return;
+        }
+
+        if(!data.command().isLeaf() && data.args().length == 0){
+            System.out.println(data.command().info());
+            return;
+        }
+
+        next.handle(ctx);
+    }
+}
