fa92f9935088b3e3a541b72276328aa82a5a9c36
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-04-04 21:43:01 -0300

    feat(wallet): add app bootstrap and new subcommands

--- src/main/java/org/example/Wallet.java
+++ src/main/java/org/example/Wallet.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import org.example.presentation.cli.commands.category.subCommands.*;
+import org.example.presentation.cli.commands.transaction.subCommands.AddTransaction;
 import org.example.presentation.cli.core.CommandGroup;
 import org.example.presentation.cli.core.CommandRegistry;
 import org.example.presentation.cli.core.CommandResolver;
@@ -9,6 +10,7 @@ import org.example.presentation.cli.core.interfaces.Command;
 import org.example.presentation.cli.core.middleware.PipelineMiddleware;
 import org.example.presentation.cli.core.records.ResolvedCommand;
 import org.example.presentation.cli.core.parser.ArgsParser;
+import org.example.application.services.TransactionService;
 import org.example.middleware.HelpMiddleware;
 import org.example.middleware.ParserMiddleware;
 import org.example.middleware.ResolverMiddleware;
@@ -17,8 +19,31 @@ import org.example.presentation.cli.commands.Hello;
 import java.util.Arrays;
 
 public class Wallet {
+
+    public static boolean isEmptyCommand(String args[]){
+        return args.length == 0 || args[0].trim().isEmpty();
+    }
+
+    public static void messageUnknownCommand(String commandName, CommandRegistry registry){
+        System.out.println("Unknown command: \"" + commandName + "\"\n");
+        messageEmptyCommand(registry);
+    }
+
+    public static void messageEmptyCommand(CommandRegistry registry){
+        System.out.println("=".repeat(50));
+        System.out.println("  WALLET Available Commands");
+        System.out.println("=".repeat(50) + "\n");
+        registry.getAllCommands().forEach((_name, cmd) -> {
+            System.out.println(cmd.info());
+            System.out.println("-".repeat(50) + "\n");
+        });
+    }
     public static void main(String[] args) {
+        App app = App.getInstance();
         CommandRegistry registry = new CommandRegistry();
+
+        app.init();
+        TransactionService transactionService = app.makeTransactionService();
 
         CommandGroup category = new CommandGroup("category");
         category.register(new AddCategory());
@@ -28,7 +53,7 @@ public class Wallet {
         category.register(new ListByNameCategory());
 
         CommandGroup transaction = new CommandGroup("transaction");
-        // TODO make crud transaction
+        transaction.register(new AddTransaction(transactionService));
 
         CommandGroup goal = new CommandGroup("goal");
         // TODO make crud goal
@@ -38,17 +63,10 @@ public class Wallet {
         registry.register(goal);
         registry.register(new Hello());
 
-        //TODO: definir o helper
-        if (args.length == 0 || args[0].trim().isEmpty()) {
-            System.out.println("=".repeat(50));
-            System.out.println("  WALLET Available Commands");
-            System.out.println("=".repeat(50) + "\n");
-            registry.getAllCommands().forEach((_name, cmd) -> {
-                System.out.println(cmd.info());
-                System.out.println("-".repeat(50) + "\n");
-            });
+        if(isEmptyCommand(args)){
+            messageEmptyCommand(registry);
             return;
-        }
+        };
 
         String commandName = args[0];
         String[] commandsArgs = Arrays.copyOfRange(args, 1, args.length);
@@ -56,10 +74,7 @@ public class Wallet {
         Command command = registry.get(commandName);
 
         if (command == null) {
-            System.out.println("Unknown command: \"" + commandName + "\"\n");
-            System.out.println("Available commands:\n");
-            registry.getAllCommands().forEach((_name, cmd) ->
-                    System.out.println(cmd.info()));
+            messageUnknownCommand(commandName, registry);
             return;
         }
 
@@ -68,10 +83,10 @@ public class Wallet {
         ArgsParser parser = new ArgsParser();
 
         pipeline
-                .add(new ResolverMiddleware(resolver))
-                .add(new HelpMiddleware())
-                .add(new ParserMiddleware(parser));
-
+        .add(new ResolverMiddleware(resolver))
+        .add(new HelpMiddleware())
+        .add(new ParserMiddleware(parser));
+        
         ResolvedCommand ctx = new ResolvedCommand(command, commandsArgs);
         pipeline.execute(ctx);
     }
