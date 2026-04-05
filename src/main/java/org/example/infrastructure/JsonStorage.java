af2d56a6a13112b9c94f53c9443ec6191ede624f
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-04-04 21:32:56 -0300

    refactor(storage/gson): adjust imports

--- src/main/java/org/example/infrastructure/JsonStorage.java
+++ src/main/java/org/example/infrastructure/JsonStorage.java
@@ -2,14 +2,6 @@ package org.example.infrastructure;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
